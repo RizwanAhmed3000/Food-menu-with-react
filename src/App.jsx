4c9034c5c34b13018639b358acf8d78b631f25a1
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-09-08 21:53:12 +0500

    food menu with react

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import './App.css';
+import Card from 'react-bootstrap/Card';
+import { useState, useEffect } from 'react';
+
+function App() {
+
+  const [menus, setMenus] = useState([]);
+
+  async function getMenu() {
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=steak`);
+    const data = await res.json();
+    console.log(data.data.recipes)
+    setMenus(data.data.recipes)
+  }
+
+  useEffect(function () {
+    getMenu()
+  }, [])
+  // getMenu()
+
+  return (
+    <div className="App">
+      <Header />
+      <SubHeader />
+      <Menu menus={menus} />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <h1 className='headerFont'>Fast React Pizza Co</h1>
+  )
+}
+function SubHeader() {
+  return (
+    <>
+      <h3>Our Menu</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia ut officia ipsum dignissimos magnam. Sit, dolores quod officia temporibus nisi, placeat tempore dicta ullam, autem amet deleniti porro aspernatur officiis?</p>
+    </>
+  )
+}
+function Menu({ menus }) {
+  return (
+    <div style={{ width: "100vw", display: "flex", alignItems: "center", justifyContent: "center", flexWrap: "wrap", marginRight: "0px" }}>
+      {
+        menus.map((menu) => (
+          <CardContainer image={menu.image_url} title={menu.title} />
+        ))
+      }
+    </div>
+  )
+}
+function Footer() {
+  return (
+    <p>We are open 24 / 7</p>
+  )
+}
+
+function CardContainer({image, title}) {
+  return (
+    <Card style={{ width: '18rem', margin: "15px" }}>
+      <Card.Img variant="top" src={image} style={{width: "200px", height: "200px", objectFit: "cover", borderRadius: "25px"}}/>
+      <Card.Body>
+        <Card.Title style={{fontSize: "1.2rem", fontWeight: "bold"}}>{title}</Card.Title>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default App;
